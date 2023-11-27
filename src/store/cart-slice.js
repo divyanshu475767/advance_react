ac11dc6530325fabd658db5dc75e4b4e544c1b1c
divyanshu4757 <guptadivyanshu400@gmail.com>
2023-11-27 15:40:38 +0530

    cart

---
+++ src/store/cart-slice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+createSlice({
+    name:'cart',
+    initialState:{
+       item:[],
+       totalQuantity:0,
+    }
+    ,
+    reducers:{
+
+    }
+})
